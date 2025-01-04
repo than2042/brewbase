27fd09b421be78c73bec4777e1c7bbc09ea5f6d6
thanthan <than.than2042@gmail.com>
2025-01-04 15:29:54 +0000

    set background, navigation and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            © BrewBase 2025
+        </div>
+    )
+}
+export default Footer
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const Navigation = () => {
+    return (
+        <div>
+            <nav>
+                <Link href={'/'}>Home</Link >
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation
